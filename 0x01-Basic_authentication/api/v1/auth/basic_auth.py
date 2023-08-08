725c83333ef10eaf26a62ec192ad9e07bc363828
H3c7o4 <hectorvladitok@gmail.com>
2023-08-08 15:33:51 +0200

    Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,8 @@
 Module for BasicAuth
 """
 from api.v1.auth.auth import Auth
+import binascii
+import base64
 
 
 class BasicAuth(Auth):
@@ -30,3 +32,29 @@ class BasicAuth(Auth):
         else:
             value = authorization_header.replace('Basic ', '')
             return value
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str
+            ) -> str:
+        """
+
+        Args:
+          - base64_authorization_header(string): base64 authorization
+
+        Returns:
+          - a String
+        """
+        if base64_authorization_header is None:
+            return None
+        elif type(base64_authorization_header) != str:
+            return None
+        
+        try:
+            data = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True
+                    )
+            return data.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
