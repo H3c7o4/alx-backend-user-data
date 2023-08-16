cd3958b40713ad19782e5e5287fd3cc340a198c5
H3c7o4 <hectorvladitok@gmail.com>
2023-08-16 15:20:46 +0200

    implement a _generate_uuid function in the auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,7 +25,7 @@ def _hash_password(password: str) -> bytes:
     return hashed_password
 
 
-def _generate_uuid():
+def _generate_uuid() -> str:
     """This function returns a string representation of a new UUID
     """
     return str(uuid.uuid4())
