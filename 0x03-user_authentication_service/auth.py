321f5c5c2b695c64635e0f1ba7e381b4580068dd
H3c7o4 <hectorvladitok@gmail.com>
2023-08-16 17:32:56 +0200

    implement the Auth.update_password method. It takes reset_token string argument and a password string argument and returns None

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -161,7 +161,7 @@ class Auth:
             user = self._db.find_user_by(reset_token=reset_token)
             h_password = _hash_password(password)
             self._db.update_user(user_id=user.id,
-                                 hashed_password=hashed_password,
+                                 hashed_password=h_password,
                                  reset_token=None)
         except NoResultFound:
             raise ValueError
