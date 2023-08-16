f161ceb06ee01e081352b7ac1727c63d1d61baa7
H3c7o4 <hectorvladitok@gmail.com>
2023-08-16 12:03:08 +0200

    Implement the add_user method

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -30,8 +30,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    @classmethod
-    def add_user(email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
 
         Args:
